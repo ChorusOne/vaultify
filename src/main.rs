78334448d619331b6d617d79b6a9b664fbf5fe0b
Patrick Jeremic <pjeremic@chorus.one>
2024-11-13 14:18:22 +0100

    add setting env

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 #[cfg(target_os = "linux")]
-fn spawn_process(detach: bool) {
+fn spawn_process(clear_env: bool, detach: bool) {
+    use std::collections::HashMap;
     use std::os::unix::process::CommandExt;
     use std::process::Command;
 
     // TODO: parse command from input and tokenize it
-    let mut command = Command::new("sleep");
-    command.arg("10");
+
+    let mut envs = HashMap::new();
+    envs.insert("TEST_VAL", "1234");
+
+    // setup command
+    let mut command = Command::new("env");
+    //command.arg("\"$TEST_VAL\"");
+
+    // set envs
+    if clear_env {
+        command.env_clear();
+    }
+    command.envs(envs);
 
     if detach {
         // setpgid to make the new process independent of this process
@@ -21,5 +33,5 @@ fn spawn_process(detach: bool) {
 }
 
 fn main() {
-    spawn_process(false)
+    spawn_process(true, false)
 }
